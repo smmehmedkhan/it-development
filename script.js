ccaa13870c167d8e1f3ab9637eab72ae693cd5ea
SHForidAhamed <134963826+SHForidAhamed@users.noreply.github.com>
2024-05-26 01:28:41 +0600

    add hamburger nav bar

---
+++ script.js
@@ -0,0 +1,20 @@
+const navMenu = document.querySelector(".nav-menu");
+const navHamMenu = document.querySelector(".nav-hamburger-menu");
+const navLinks = document.querySelectorAll(".nav-link");
+const navHamBtn = document.getElementById("nav-ham-btn");
+const navCloseBtn = document.getElementById("nav-close-btn");
+
+navHamBtn.addEventListener("click", () => {
+    navMenu.classList.add("show");
+    navHamMenu.classList.add("hide");
+})
+
+function backToDefault() {
+    navMenu.classList.remove("show");
+    navHamMenu.classList.remove("hide");
+}
+
+navCloseBtn.addEventListener("click", backToDefault);
+navLinks.forEach( button => {
+    button.addEventListener("click", backToDefault);
+})
