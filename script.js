aa509b15373bd582c84e48127478cb49103f4c23
SHForidAhamed <134963826+SHForidAhamed@users.noreply.github.com>
2024-05-27 12:05:20 +0600

    don't know what happend...

--- script.js
+++ script.js
@@ -3,17 +3,15 @@ const navHamMenu = document.querySelector(".nav-hamburger-menu");
 const navLinks = document.querySelectorAll(".nav-link");
 const navHamBtn = document.getElementById("nav-ham-btn");
 const navCloseBtn = document.getElementById("nav-close-btn");
-
+// Functions....
 navHamBtn.addEventListener("click", () => {
     navMenu.classList.add("show");
     navHamMenu.classList.add("hide");
 })
-
 function backToDefault() {
     navMenu.classList.remove("show");
     navHamMenu.classList.remove("hide");
 }
-
 navCloseBtn.addEventListener("click", backToDefault);
 navLinks.forEach( button => {
     button.addEventListener("click", backToDefault);
